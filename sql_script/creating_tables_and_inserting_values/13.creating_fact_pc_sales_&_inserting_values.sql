cc9def1d4ede734171cd46ed7da73184fd62970f
Mzimkhulu Mthethwa <housekhulu@gmail.com>
2026-05-04 19:21:57 -0700

    Updating my code on the fact_sales table and the dim_shop table

--- sql_script/creating_tables_and_inserting_values/13.creating_fact_pc_sales_&_inserting_values.sql
+++ sql_script/creating_tables_and_inserting_values/13.creating_fact_pc_sales_&_inserting_values.sql
@@ -2,10 +2,20 @@
 --creating fact_pc_sales tabe with the primary key
 -------------------------------------------------
 
-DROP TABLE [computer_staging].[dbo].[fact_pc_sales];
+--DROP TABLE [computer_staging].[dbo].[fact_pc_sales];
 
 CREATE TABLE [computer_staging].[dbo].[fact_pc_sales](
     [Sales_ID] INT IDENTITY (1,1) PRIMARY KEY NOT NULL,
+	[customer_id] INT NULL,
+    [date_id] INT NULL,
+    [Payment_ID] INT NULL,
+    [PC_ID] INT NULL,
+    [Priority_ID] INT NULL,
+    [Sales_person_id] INT NULL,
+    [Shop_Id] INT NULL,
+    [Storage_ID] INT NULL,
+    [location_id] INT NULL,
+    [channel_ID] INT NULL,
 	[Cost_Price] INT NOT NULL,
 	[Sale_Price] INT NOT NULL,
 	[Discount_Amount] INT NOT NULL,
@@ -13,9 +23,21 @@ CREATE TABLE [computer_staging].[dbo].[fact_pc_sales](
 	[Credit_Score] INT NOT NULL,
 	[Cost_of_Repairs] INT NOT NULL,
 	[Total_Sales_per_Employee] INT NOT NULL,
-	[PC_Market_Price] INT NOT NULL
-);
+	[PC_Market_Price] INT NOT NULL,
+ 
 
+  
+
+CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES [computer_staging].[dbo].[dim_customer] (customer_id),
+CONSTRAINT fk_date FOREIGN KEY (date_id) REFERENCES [computer_staging].[dbo].[dim_date] (date_id),
+CONSTRAINT fk_payment FOREIGN KEY (Payment_ID) REFERENCES [computer_staging].[dbo].[dim_payment] (payment_id),
+CONSTRAINT fk_PC FOREIGN KEY (PC_ID) REFERENCES [computer_staging].[dbo].[dim_pc] (PC_ID),
+CONSTRAINT fk_Priority FOREIGN KEY (Priority_ID) REFERENCES [computer_staging].[dbo].[dim_Priority] (Priority_ID),
+CONSTRAINT fk_Sales_person FOREIGN KEY (Sales_person_id) REFERENCES [computer_staging].[dbo].[dim_Salesperson] (Sales_person_id),
+CONSTRAINT fk_Shop FOREIGN KEY (Shop_Id) REFERENCES [computer_staging].[dbo].[dim_Shop] (Shop_Id),
+CONSTRAINT fk_Storage FOREIGN KEY (Storage_ID) REFERENCES [computer_staging].[dbo].[dim_Storage] (Storage_ID),
+CONSTRAINT fk_location FOREIGN KEY (location_id) REFERENCES [computer_staging].[dbo].[dim_location] (location_id),
+CONSTRAINT fk_channel FOREIGN KEY (channel_ID) REFERENCES [computer_staging].[dbo].[dim_channel] (channel_ID) );
 
 -----------------------------------------
 --Inserting values into the fact_pc_sales
--- sql_script/stored_procedures/9.creating_dim_shop.sql
+++ sql_script/stored_procedures/9.creating_dim_shop.sql
@@ -10,10 +10,8 @@ BEGIN
 --creating dim_shop tabe with the primary key
 ---------------------------------------------
 
-DROP TABLE [computer_staging].[dbo].[dim_shop];
-
 CREATE TABLE [computer_staging].[dbo].[dim_shop](
-    [Shop_Id] INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    [Shop_id] INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
 	[Shop_Name] [nvarchar](250) NOT NULL,
 	[Shop_Age] [nvarchar](250) NOT NULL
 );
@@ -30,5 +28,5 @@ INSERT INTO [computer_staging].[dbo].[dim_shop](
 SELECT DISTINCT 
             [Shop_Name],
 			[Shop_Age]
-FROM [computer_staging].[dbo].[raw_pc_data];
+FROM [computer_staging].[dbo].[raw_pc_data];;
 END;
